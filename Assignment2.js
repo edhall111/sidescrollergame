a321ddff63af1759eeb4a861129168719cfc2689
Edward Hall <edhall211@gmail.com>
2016-10-26 14:19:56 -0400

    Added user ability to destroy enemy ships

--- Assignment2.js
+++ Assignment2.js
@@ -11,11 +11,16 @@ var graphicsContext = canvas.getContext('2d');
 //variable that will hold the image of the background
 var background = new Image();
 var enemySprite = new Image();
+var laser = new Image();
 var playerAlive = true;
 var jetY=225;
 var deltaY = 0;
 var newEnemy;
-var enemyX = 977;
+var newShot;
+var newPlayerShot;
+var enemies = [];
+var score = 0;
+var shotList = [];
 /**
  * loadImages will load all of our images, setting them with the sprites that they will represent
  * SOURCE: SimpleMove.js, as given to us in class
@@ -26,6 +31,7 @@ function loadImages() {
     sprite.src = 'images/jet.png';
     enemySprite.src = 'images/ship.png';
     missile.src = 'images/missile.png';
+    laser.src = 'images/laser.png';
 }
 function updateBackgroundOffset(currentTime){
     backgroundDx = backgroundDx +2;
@@ -44,38 +50,109 @@ function drawBackground(currentTime) {
     graphicsContext.translate(backgroundDx, 0);
 }
 
+function Enemy(size, x, y){
+    this.enemySize = size;
+    this.enemyX = x;
+    this.enemyY = y;
+}
 
-
+function Shot(type, x, y){
+    this.shotType = type;
+    this.shotX = x;
+    this.shotY = y;
+}
+var timeForEnemy = true;
 /**
  * drawSprite is the bulkiest method, as it will handle the drawing of all sprites within the game(harambe[the player], banana, and the three SWAT enemies)
  * Along with this, it will handle the collision detection of the sprites, deciding whether a user has scored a point or lost the game.
  * */
 function drawPlayer(currentTime){
     if(playerAlive == false){
-        //print death message
+        document.getElementById("score").innerHTML = "YOU HAVE FAILED...FINAL SCORE: "+score;
     }
     else{
-        spriteMove();
         graphicsContext.drawImage(sprite, 20, jetY);
-        graphicsContext.drawImage(enemySprite, enemyX, 100);
+        if(enemies.length < 6) {
+            newEnemy = new Enemy('small', 977, Math.floor(Math.random() * 506) + 1);
+            enemies.push(newEnemy);
+        }
+        if(enemies.length > 6){
+            enemies.length = 6;
+        }
+        for (i = 0; i < enemies.length; i++) {
+            graphicsContext.drawImage(enemySprite, enemies[i].enemyX, enemies[i].enemyY);
+        }
+
+        for(j = 0; j < enemies.length; j++) {
+            if(enemies[j].enemyX < 0){
+                enemies.splice(j, 1);
+            }
+            if(enemies[j].enemySize == 'small'){
+                if(enemies[j].enemyX == 977 || enemies[j].enemyX == 977*0.8 ||
+                    enemies[j].enemyX == 977 * 0.6 || enemies[j].enemyX == 977 * 0.4 || enemies[j].enemyX == 977 * 0.2){
+                    //shoot once
+                    newShot = new Shot('enemy', enemies[j].enemyX, enemies[j].enemyY+22);
+                    shotList.push(newShot);
+                }
+            }
+            var rectEnemy = {x: enemies[j].enemyX, y: enemies[j].enemyY, width: 50, height: 44};
+            for(l = 0; l < shotList.length; l++){
+                var rectShot = {x: shotList[l].shotX, y: shotList[l].shotY, width: 40, height: 20};
+                if(rectEnemy.x < rectShot.x + rectShot.width &&
+                    rectEnemy.x + rectEnemy.width > rectShot.x &&
+                    rectEnemy.y < rectShot.y + rectShot.height &&
+                    rectEnemy.height + rectEnemy.y > rectShot.y){
+                    if(shotList[l].shotType == 'player'){
+                        score++;
+                        enemies.splice(j, 1);
+                        shotList.splice(l, 1);
+                    }
+
+
+                }
+            }
+            /*else if (enemies[j].enemySize == 'large'){
+                if(enemies[j].enemyX == 977 || enemies[j].enemyX == 977*0.9 ||
+                    enemies[j].enemyX == 977 * 0.8 || enemies[j].enemyX == 977 * 0.7 || enemies[j].enemyX == 977 * 0.6 ||
+                    enemies[j].enemyX == 977 * 0.5 || enemies[j].enemyX == 977* 0.4 ||
+                    enemies[j].enemyX == 977 * 0.3 || enemies[j].enemyX == 977 * 0.2 || enemies[j].enemyX == 977 * 0.1){
+                    //shoot once
+                }
+            }*/
+
+        }
+        //draw shots
+        for(k = 0; k <shotList.length; k++){
+            if(shotList[k].shotType == 'player'){
+                graphicsContext.drawImage(missile, shotList[k].shotX, shotList[k].shotY);
+            }
+            else if(shotList[k].shotType ==  'enemy'){
+                graphicsContext.drawImage(laser, shotList[k].shotX, shotList[k].shotY);
+            }
+        }
     }
 
 }
-function drawEnemies(currentTime) {
 
-    graphicsContext.drawImage(enemySprite, enemyX, 300);
-}
+var bigEnemyTicker = 0;
+
 /**
  * naiveGameLoop will loop while the game is running, altering the coordinates of sprites as they move around the screen.
  * */
-function spriteMove(){
+function spriteMove(currentTime){
     jetY = jetY + deltaY;
+    for (i = 0; i < enemies.length; i++){
+        enemies[i].enemyX = enemies[i].enemyX - 2;
+    }
 
-    enemyX = enemyX -1;
-    //newEnemy.enemyX = newEnemy.enemyX - 3;
-    //for (i = 0; i < enemies.length; i++){
-      //  enemies[i].enemyX = enemies[i].enemyX - 3;
-    //}
+    for (j = 0; j < shotList.length; j++){
+        if(shotList[j].shotType == 'player'){
+            shotList[j].shotX = shotList[j].shotX + 3;
+        }
+        else{
+            shotList[j].shotX = shotList[j].shotX - 3;
+        }
+    }
 }
 /**
  * handleKeyUp will set our deltaY and deltaX to 0 whenever the arrow keys are released.
@@ -107,18 +184,14 @@ function handleKeyDown(keyboardEvent){
             console.log("up");
             deltaY = -10;
             break;
-        case (keyboardEvent.keyCode == 32):
-            console.log("space");
-            if (shotCheck == 0) {
-                drawMissile(jetY);
+        case 'ArrowRight':
+            if(shotCheck == 0){
+                newPlayerShot = new Shot('player', 20, jetY+18);
+                shotList.push(newPlayerShot);
                 shotCheck = 1;
             }
             break;
     }
-}
-
-function drawMissile(jetYPos){
-        graphicsContext.drawImage(missile, 20, jetYPos);
 }
 /**
  * drawComponents() will draw our background on the canvas as well as our sprites
@@ -128,6 +201,7 @@ function drawComponents(currentTime) {
     updateBackgroundOffset(currentTime);
     drawBackground(currentTime);
     drawPlayer(currentTime);
+    spriteMove(currentTime);
     //drawEnemies(currentTime);
     requestAnimationFrame(drawComponents);
 }
